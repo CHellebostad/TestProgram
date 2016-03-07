ad693cbe665167bbb26afdcb7379185d644da573
Christian-Skule <Christian_hellebostad@msn.com>
2016-03-07 17:54:02 +0100

    Video

--- TestProgram/TestProgram/TestProgram.cpp
+++ TestProgram/TestProgram/TestProgram.cpp
@@ -3,6 +3,8 @@
 
 #include "stdafx.h"
 #include <opencv2\opencv.hpp>
+#include <iostream>
+#include <fstream>
 
 using namespace System;
 using namespace System::IO::Ports;
@@ -10,11 +12,16 @@ using namespace System::Threading;
 using namespace std;
 using namespace cv;
 
+ofstream out("out.txt");
 float gyroX;
+float gyroY;
 float gyroZ;
 float gyroXcorr;
+float gyroYcorr;
 float gyroZcorr;
 bool first = true;
+VideoCapture cap;
+VideoWriter writer;
 
 void static gyroData() {
 	SerialPort port;
@@ -24,7 +31,7 @@ void static gyroData() {
 	float f;
 	char b[4];
 	while (true) {
-		for (int x = 0; x < 2; x++) {
+		for (int x = 0; x < 3; x++) {
 			for (int i = 0; i < 4; i++) {
 				b[i] = port.ReadByte();
 			}
@@ -33,33 +40,53 @@ void static gyroData() {
 				if (first) {
 					gyroXcorr = gyroX;
 				}
-				cout << "GyroX: " << gyroX-gyroXcorr;
+				out << gyroX-gyroXcorr <<" ";
 			}
 			else if (x == 1) {
+				memcpy(&gyroY, b, sizeof(gyroY));
+				if (first) {
+					gyroYcorr = gyroY;
+				}
+				out << gyroY-gyroYcorr<<" ";
+			}
+			else if (x == 2) {
 				memcpy(&gyroZ, b, sizeof(gyroZ));
 				if (first) {
 					gyroZcorr = gyroZ;
 				}
-				cout << " GyroZ: " << gyroZ-gyroZcorr << endl;
+				out << gyroZ - gyroZcorr << endl;
 			}
 		}
 		if (first) {
 			first = !first;
 		}
 	}
+	out.close();
 
 }
 
 
 
+
+
 int main()
 {
-
-	Thread^ t0 = gcnew Thread(gcnew ThreadStart(gyroData));
-	t0->Start();
+	Mat frame;
+	cap.open(0);
+	writer.open("D:\\Kalibrering.avi",
+		cap.get(CAP_PROP_FOURCC),
+		cap.get(CAP_PROP_FPS),
+		Size(cap.get(CAP_PROP_FRAME_WIDTH),cap.get(CAP_PROP_FRAME_HEIGHT)),true);
+	
+	//Thread^ t0 = gcnew Thread(gcnew ThreadStart(gyroData));
+	//t0->Start();
 	while (true) {
-
+		cap.read(frame);
+		imshow("frame",frame);
+		writer.write(frame);
+		if (waitKey(30) >= 0) break;
 	}
+	writer.release();
 
 	return 0;
 }
