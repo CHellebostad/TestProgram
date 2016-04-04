59ffca3cdf33a9ea3e666f520ce90e8e55552ece
Christian-Skule <Christian_hellebostad@msn.com>
2016-04-04 12:10:36 +0200

    tull

--- TestProgram/TestProgram/TestProgram.cpp
+++ TestProgram/TestProgram/TestProgram.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <fstream>
 #include <opencv2\videoio.hpp>
+#include <ctime>
 
 using namespace System;
 using namespace System::IO::Ports;
@@ -29,15 +30,19 @@ Mat Kintrisicinvers(3, 3, CV_32FC1);
 
 
 
+
 void static gyroData() {
 	SerialPort port;
-	port.BaudRate = 115200;
+	port.BaudRate = 250000;
 	port.PortName = "COM3";
 	port.Open();
 	float f;
 	char b[4];
 	while (true) {
+		out << clock() << ",";
+		out << 3 << ",";
 		for (int x = 0; x < 3; x++) {
+			
 			for (int i = 0; i < 4; i++) {
 				b[i] = port.ReadByte();
 			}
@@ -46,22 +51,23 @@ void static gyroData() {
 				if (first) {
 					gyroXcorr = gyroX;
 				}
-				out << gyroX-gyroXcorr <<" ";
+				out << gyroX - gyroXcorr << ",";
 			}
 			else if (x == 1) {
 				memcpy(&gyroY, b, sizeof(gyroY));
 				if (first) {
 					gyroYcorr = gyroY;
 				}
-				out << gyroY-gyroYcorr<<" ";
+				out << gyroY - gyroYcorr << ",";
 			}
 			else if (x == 2) {
 				memcpy(&gyroZ, b, sizeof(gyroZ));
 				if (first) {
 					gyroZcorr = gyroZ;
 				}
-				out << gyroZ - gyroZcorr << endl;
+				out << gyroZ - gyroZcorr << ","<< endl;
 			}
+			
 		}
 		if (first) {
 			first = !first;
@@ -75,14 +81,15 @@ void static gyroData() {
 }
 
 void cropimage() {
-	
+
 	while (true) {
 		cap >> frame;
-		Mat rotx_mat = getRotationMatrix2D(Point(frame.cols / 2, frame.rows / 2),30,1);
+		float graderX = (180 / 3.14)*gyroY;
+		Mat rotx_mat = getRotationMatrix2D(Point(frame.cols / 2, frame.rows / 2), -gyroX, 1);
 		Mat warp;
-		warpAffine(frame, warp, rotx_mat,Size(frame.rows,frame.cols));
-		//Rect roi(20, 20, 600,380);
-		//Mat roi_frame = warp(roi);
+		warpAffine(frame, warp, rotx_mat, Size(frame.cols, frame.rows));
+		/*Rect roi(40, 40, 580,400);
+		warp(roi);*/
 		imshow("roi_frame", warp);
 		char c = (char)waitKey(33);
 		if (c == 27) break;
@@ -98,7 +105,7 @@ void captureVideo() {
 
 		cap >> frame;
 		video.write(frame);
-		imshow("frame", frame);
+		//imshow("frame", frame);
 		char c = (char)waitKey(33);
 		if (c == 27) break;
 	}
@@ -107,12 +114,15 @@ void captureVideo() {
 
 int main()
 {
-	cap.open(0);
+	cap.open(1);
+	//cap.set(CAP_PROP_FRAME_HEIGHT, 400);
+	//cap.set(CAP_PROP_FRAME_WIDTH, 600);
 
-
-	/*Thread^ t0 = gcnew Thread(gcnew ThreadStart(gyroData));
-	t0->Start();*/
-	Thread^ t1 = gcnew Thread(gcnew ThreadStart(cropimage));
-	t1->Start();
+	Thread^ t0 = gcnew Thread(gcnew ThreadStart(gyroData));
+	t0->Start();
+	Thread^ t1 = gcnew Thread(gcnew ThreadStart(captureVideo));
+	//t1->Start();
+	//Thread^ t1 = gcnew Thread(gcnew ThreadStart(cropimage));
+	//t1->Start();
 	return 0;
 }
