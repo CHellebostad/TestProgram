832f2196c0c7f9c6c8e342ddcd2e48b951fb3266
christian_hellebostad <christian_hellebostad@msn.com>
2016-04-20 07:38:39 +0200

    sdfgs

--- TestProgram/TestProgram/TestProgram.cpp
+++ TestProgram/TestProgram/TestProgram.cpp
@@ -67,39 +67,80 @@ void cropimage() {
 }
 
 void removeFishEye() {
-	int numBoards = 0;
-	int numCornersHor=9;
-	int numCornersVer=6;
-	int numSquares = numCornersHor*numCornersVer;
-	Size bord_sz = Size(numCornersHor, numCornersVer);
-	vector<vector<Point3f>> object_points;
-	vector<vector<Point2f>> image_points;
-	vector<Point2f> corners;
-	Mat image;
-	Mat gray_image;
-	Mat intrinsic = Mat(3, 3, CV_32FC1);
-	Mat distCoeffs = Mat(2, 2, CV_32FC1);
-	vector<Mat> rvecs;
-	vector<Mat> tvecs;
-	intrinsic.ptr<float>(0)[0] = 1520.285040118956;
-	intrinsic.ptr<float>(1)[1] = 1501.931438021879;
+	//int numBoards = 20;
+	//int numCornersHor=9;
+	//int numCornersVer=6;
+	//int numSquares = numCornersHor*numCornersVer;
+	//Size board_sz = Size(numCornersHor, numCornersVer);
+
+	//vector<vector<Point3f>> object_points;
+	//vector<vector<Point2f>> image_points;
+	//vector<Point2f> corners;
+	//int successes = 0;
+
+	//Mat gray_image;
+	
+	
+
+	//vector<Point3f> obj;
+	//for (int j = 0; j < numSquares; j++) {
+	//	obj.push_back(Point3f(j / numCornersHor, j%numCornersHor, 0.0f));
+	//}
+	//while (successes < numBoards) {
+	//	cvtColor(image, gray_image, CV_BGR2GRAY);
+	//	bool found = findChessboardCorners(image, board_sz, corners, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
+	//	if (found) {
+	//		cornerSubPix(gray_image, corners, Size(11, 11), Size(-1, -1), TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+	//		drawChessboardCorners(gray_image, board_sz, corners, found);
+	//	}
+
+	//	imshow("win1", image);
+	//	imshow("win2", gray_image);
+	//	cap >> image;
+	//	char c = (char)waitKey(33);
+	//	if (c == 27) break;
+
+	//	if (c == ' ' && found != 0) {
+	//		image_points.push_back(corners);
+	//		object_points.push_back(obj);
+	//		printf("Snap stored!");
+	//		successes++;
+	//		if (successes >= numBoards)
+	//			break;
+	//	}
+	//}
+	//Mat intrinsic = Mat(3, 3, CV_32FC1);
+	//Mat distCoeffs = Mat(2, 2, CV_32FC1);
+	//vector<Mat> rvecs;
+	//vector<Mat> tvecs;
+	//intrinsic.ptr<float>(0)[0] = 1520.285040118956;
+	//intrinsic.ptr<float>(1)[1] = 1501.931438021879;
 	//intrinsic.ptr<float>(0)[2] = 1295.589821181276;
 	//intrinsic.ptr<float>(1)[2] = 944.1007834224537;
 	//intrinsic.ptr<float>(2)[2] = 1;
 
-	frame = imread("img2.jpg");
-	Mat rectifisert;
-	undistort(frame, rectifisert, intrinsic, distCoeffs);
-	imshow("test",rectifisert);
+	//frame = imread("img2.jpg");
+	//Mat rectifisert;
+	//undistort(frame, rectifisert, intrinsic, distCoeffs);
+	//imshow("test",rectifisert);
 }
 
 
 
 int main()
 {
-	removeFishEye();
+	cap.open(0);
+	Mat image ;
 
-	//cap.open(0);
+	while (true) {
+		//cap >> image;
+		//namedWindow("test", WINDOW_AUTOSIZE);
+		//imshow("test", image);
+		//if (waitKey(30) >= 0) break;
+	}
+	
+	//removeFishEye();
+
 	//Thread^ t0 = gcnew Thread(gcnew ThreadStart(gyroData));
 	//t0->Start();
 	//Thread^ t1 = gcnew Thread(gcnew ThreadStart(cropimage));
